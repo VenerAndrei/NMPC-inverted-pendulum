954f635ea8a26e988ef9add000dc0383b97c1857
VenerAndrei <venerandrei331@gmail.com>
2024-06-06 22:50:04 +0300

    Added LQRv2

---
+++ MatlabArduinoCode/LQR_v1/LQRv1_Data.m
@@ -0,0 +1,62 @@
+% Define the filename
+filename = 'LQRv1_Data.log';
+
+% Open the file
+fileID = fopen(filename, 'r');
+
+% Read the data
+data = fscanf(fileID, '%f %f %f %f', [4 Inf]);
+
+% Close the file
+fclose(fileID);
+
+% Transpose the data to get columns
+data = data';
+
+% Separate the data into individual variables
+position = data(:, 1);
+velocity = data(:, 2);
+pendulum_angle = data(:, 3);
+angular_velocity = data(:, 4);
+
+% Define the time array
+time_interval = 0.02; % seconds
+num_readings = size(data, 1);
+time = (0:num_readings-1) * time_interval;
+
+% Plot the data against the timeline
+figure;
+
+subplot(4, 1, 1);
+plot(time, position, '-o');
+title('Position vs. Time');
+xlabel('Time (s)');
+ylabel('Position (cm)');
+
+subplot(4, 1, 2);
+plot(time, velocity, '-o');
+title('Velocity vs. Time');
+xlabel('Time (s)');
+ylabel('Velocity (cm/s)');
+
+subplot(4, 1, 3);
+plot(time, pendulum_angle, '-o');
+title('Pendulum Angle vs. Time');
+xlabel('Time (s)');
+ylabel('Pendulum Angle (rad)');
+
+subplot(4, 1, 4);
+plot(time, angular_velocity, '-o');
+title('Angular Velocity vs. Time');
+xlabel('Time (s)');
+ylabel('Angular Velocity (rad/s)');
+
+% Display the parsed data (optional)
+disp('Position:');
+disp(position);
+disp('Velocity:');
+disp(velocity);
+disp('Pendulum Angle:');
+disp(pendulum_angle);
+disp('Angular Velocity:');
+disp(angular_velocity);
